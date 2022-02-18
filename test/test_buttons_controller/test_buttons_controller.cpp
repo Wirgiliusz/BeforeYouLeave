23696b3db203286b3d017d1e0404ab38e118e906
Wirgiliusz <kamx98@gmail.com>
2022-02-18 14:56:14 +0100

    Add overrideToggle() test

--- test/test_buttons_controller/test_buttons_controller.cpp
+++ test/test_buttons_controller/test_buttons_controller.cpp
@@ -64,6 +64,23 @@ void test_override_mixed() {
     TEST_ASSERT_TRUE(buttons_controller.override_right);
 }
 
+void test_override_toggle() {
+    struct ButtonsController buttons_controller;
+    buttonsControllerInit(&buttons_controller);
+
+    overrideTurnOn(&buttons_controller, BTN_LEFT);
+    overrideTurnOff(&buttons_controller, BTN_MID);
+    overrideTurnOn(&buttons_controller, BTN_RIGHT);
+
+    overrideToggle(&buttons_controller, BTN_LEFT);
+    overrideToggle(&buttons_controller, BTN_MID);
+    overrideToggle(&buttons_controller, BTN_RIGHT);
+
+    TEST_ASSERT_FALSE(buttons_controller.override_left);
+    TEST_ASSERT_TRUE(buttons_controller.override_mid);
+    TEST_ASSERT_FALSE(buttons_controller.override_right);
+}
+
 void setup() {
     delay(2000); // NOTE!!! Wait for >2 secs if board doesn't support software reset via Serial.DTR/RTS
     UNITY_BEGIN();    // Start unit testing
@@ -72,6 +89,7 @@ void setup() {
     RUN_TEST(test_override_true_after_turn_on);
     RUN_TEST(test_override_false_after_turn_off);
     RUN_TEST(test_override_mixed);
+    RUN_TEST(test_override_toggle);
 
 
     UNITY_END(); // Stop unit testing
