03eb6ddda036afe434c46e1bf9c0e99aca1ad262
Wirgiliusz <kamx98@gmail.com>
2022-02-18 14:52:51 +0100

    Add test for overrideTurnOff()

--- lib/buttons_controller/buttons_controller.cpp
+++ lib/buttons_controller/buttons_controller.cpp
@@ -1,7 +1,7 @@
 #include "buttons_controller.h"
 
 
-static bool readButton(uint8_t button);
+static bool isButtonPressed(uint8_t button);
 static bool debounceButton(uint8_t button);
 
 void overrideTurnOn(struct ButtonsController *buttons_controller, uint8_t button) {
@@ -33,11 +33,7 @@ void buttonsControllerInit(struct ButtonsController *buttons_controller) {
 }
 
 void checkButtonsAndToggleOverride(struct ButtonsController *buttons_controller) {
-    buttons_controller->toggle_override_left = readButton(BTN_LEFT);
-    buttons_controller->toggle_override_mid = readButton(BTN_MID);
-    buttons_controller->toggle_override_right = readButton(BTN_RIGHT);
-    
-    if (buttons_controller->toggle_override_left) {
+    if (isButtonPressed(BTN_LEFT)) {
         Serial.print("Toggled override for left hook.. Current status: ");
         if (buttons_controller->override_left) {
             buttons_controller->override_left = false;
@@ -47,7 +43,7 @@ void checkButtonsAndToggleOverride(struct ButtonsController *buttons_controller)
         Serial.println(buttons_controller->override_left);
     }
 
-    if (buttons_controller->toggle_override_mid) {
+    if (isButtonPressed(BTN_MID)) {
         Serial.print("Toggled override for middle hook.. Current status: ");
         if (buttons_controller->override_mid) {
             buttons_controller->override_mid = false;
@@ -57,7 +53,7 @@ void checkButtonsAndToggleOverride(struct ButtonsController *buttons_controller)
         Serial.println(buttons_controller->override_mid);
     }
 
-    if (buttons_controller->toggle_override_right) {
+    if (isButtonPressed(BTN_RIGHT)) {
         Serial.print("Toggled override for right hook.. Current status: ");
         if (buttons_controller->override_right) {
             buttons_controller->override_right = false;
@@ -68,7 +64,7 @@ void checkButtonsAndToggleOverride(struct ButtonsController *buttons_controller)
     }
 }
 
-bool readButton(uint8_t button) {
+static bool isButtonPressed(uint8_t button) {
     static bool last_pressed_status[3] = {false, false, false};
     size_t idx = 0;
 
--- test/test_buttons_controller/test_buttons_controller.cpp
+++ test/test_buttons_controller/test_buttons_controller.cpp
@@ -38,6 +38,32 @@ void test_override_true_after_turn_on() {
     TEST_ASSERT_TRUE(buttons_controller.override_right);
 }
 
+void test_override_false_after_turn_off() {
+    struct ButtonsController buttons_controller;
+    buttonsControllerInit(&buttons_controller);
+
+    overrideTurnOff(&buttons_controller, BTN_LEFT);
+    overrideTurnOff(&buttons_controller, BTN_MID);
+    overrideTurnOff(&buttons_controller, BTN_RIGHT);
+
+    TEST_ASSERT_FALSE(buttons_controller.override_left);
+    TEST_ASSERT_FALSE(buttons_controller.override_mid);
+    TEST_ASSERT_FALSE(buttons_controller.override_right);
+}
+
+void test_override_mixed() {
+    struct ButtonsController buttons_controller;
+    buttonsControllerInit(&buttons_controller);
+
+    overrideTurnOn(&buttons_controller, BTN_LEFT);
+    overrideTurnOff(&buttons_controller, BTN_MID);
+    overrideTurnOn(&buttons_controller, BTN_RIGHT);
+
+    TEST_ASSERT_TRUE(buttons_controller.override_left);
+    TEST_ASSERT_FALSE(buttons_controller.override_mid);
+    TEST_ASSERT_TRUE(buttons_controller.override_right);
+}
+
 void setup() {
     delay(2000); // NOTE!!! Wait for >2 secs if board doesn't support software reset via Serial.DTR/RTS
     UNITY_BEGIN();    // Start unit testing
