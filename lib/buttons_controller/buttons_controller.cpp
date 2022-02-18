751438aed23672cc5ceb45fb6b6905c316ec40f4
Wirgiliusz <kamx98@gmail.com>
2022-02-18 14:16:51 +0100

    Create buttons_controller lib

---
+++ lib/buttons_controller/buttons_controller.cpp
@@ -0,0 +1,7 @@
+#include "buttons_controller.h"
+
+void buttonsControllerInit() {
+    pinMode(BTN_LEFT, INPUT);
+    pinMode(BTN_MID, INPUT);
+    pinMode(BTN_RIGHT, INPUT);
+}
---
+++ lib/buttons_controller/buttons_controller.h
@@ -0,0 +1,8 @@
+#pragam once
+#include <Arduino.h>
+
+#define BTN_LEFT D2
+#define BTN_MID D1
+#define BTN_RIGHT D0
+
+void buttonsControllerInit();
