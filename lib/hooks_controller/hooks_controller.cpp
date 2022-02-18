d8aebc44c0d32996347c26dae57e611052320965
Wirgiliusz <kamx98@gmail.com>
2022-02-18 15:15:42 +0100

    Pass test for hooks controller initialization

--- lib/hooks_controller/hooks_controller.cpp
+++ lib/hooks_controller/hooks_controller.cpp
@@ -1,1 +1,12 @@
 #include "hooks_controller.h"
+
+
+void hooksControllerInit(struct HooksController *hooks_controller) {
+    pinMode(HOOK_LEFT, INPUT);
+    pinMode(HOOK_MID, INPUT);
+    pinMode(HOOK_RIGHT, INPUT);
+
+    hooks_controller->missing_item_left = false;    
+    hooks_controller->missing_item_mid = false;    
+    hooks_controller->missing_item_right = false;    
+}
--- lib/hooks_controller/hooks_controller.h
+++ lib/hooks_controller/hooks_controller.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <Arduino.h>
+
+#define HOOK_LEFT D6
+#define HOOK_MID D7
+#define HOOK_RIGHT D8
 
 
 struct HooksController {
@@ -6,3 +11,5 @@ struct HooksController {
     bool missing_item_mid;
     bool missing_item_right;
 };
+
+void hooksControllerInit(struct HooksController *hooks_controller);
